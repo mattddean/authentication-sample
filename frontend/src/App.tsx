8163a4f3dc15b09ec41892079800b45c80ebc774
Matt Dean <mdean400@gmail.com>
2021-03-14 11:48:53 -0600

    Use DropButton for top menu

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Grommet, Heading, Button, Box } from "grommet";
+import { Grommet, Heading, Box, DropButton } from "grommet";
 import { AppBar } from "./components/AppBar";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import { Login } from "./pages/Login";
 import { User } from "./pages/User";
 import { CreateAccount } from "./pages/CreateAccount";
+import { DropButtonLink } from "./components/DropButtonLink";
 
 const theme = {
   global: {
@@ -19,29 +20,40 @@ const theme = {
 function App() {
   return (
     <Grommet theme={theme} full>
-      <AppBar>
-        <Heading level="3" margin="none">
-          Authentication Sample
-        </Heading>
-        <Button label="User's Name" onClick={() => {}} />
-      </AppBar>
-      <Box direction="row" flex overflow={{ horizontal: "hidden" }}>
-        <Box flex align="center" justify="center">
-          <Router>
+      <Router>
+        <AppBar>
+          <Heading level="3" margin="none">
+            Authentication Sample
+          </Heading>
+          <DropButton
+            label="User's Name"
+            dropAlign={{ top: "bottom", right: "right" }}
+            dropContent={
+              <Box>
+                <DropButtonLink label="Logout"></DropButtonLink>
+                <Link to="/profile">
+                  <DropButtonLink label="Profile"></DropButtonLink>
+                </Link>
+              </Box>
+            }
+          />
+        </AppBar>
+        <Box direction="row" flex overflow={{ horizontal: "hidden" }}>
+          <Box flex align="center" justify="center">
             <Switch>
               <Route exact path="/">
                 <Login />
               </Route>
-              <Route path="/user">
+              <Route path="/profile">
                 <User />
               </Route>
               <Route path="/signup">
                 <CreateAccount />
               </Route>
             </Switch>
-          </Router>
+          </Box>
         </Box>
-      </Box>
+      </Router>
     </Grommet>
   );
 }
--- frontend/src/pages/CreateAccount.tsx
+++ frontend/src/pages/CreateAccount.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Form, FormField, Button, Box, TextInput } from "grommet";
+import { NavLink as Link } from "react-router-dom";
 
 export const CreateAccount = () => {
   const [value, setValue] = React.useState({});
@@ -21,6 +22,11 @@ export const CreateAccount = () => {
       <FormField name="name" htmlFor="text-input-id" label="Password">
         <TextInput id="text-input-id" name="name" />
       </FormField>
+      <Box direction="row" gap="medium">
+        <Link to="/">
+          <small>Already have an account? Sign in</small>
+        </Link>
+      </Box>
       <Box direction="row" gap="medium">
         <Button type="submit" primary label="Submit" />
       </Box>
