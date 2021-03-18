91b7f653dee450ca27b225d5e619886d77e2f535
Matt Dean <mdean400@gmail.com>
2021-03-17 21:14:58 -0600

    Set up AWS certificate for mongo connection

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -6,11 +6,12 @@ const {
   MONGO_HOST = "localhost",
   MONGO_PORT = "27017",
   MONGO_DATABASE = "auth",
+  MONGO_CONN_STRING_ENDING="/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`"
 } = process.env;
 
 export const MONGO_URI = `mongodb://${MONGO_USERNAME}:${encodeURIComponent(
   MONGO_PASSWORD
-)}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`;
+)}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}${MONGO_CONN_STRING_ENDING}`;
 
 export const MONGO_OPTIONS: ConnectionOptions = {
   useNewUrlParser: true,
