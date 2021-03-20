7c9adfeb8c4f2859bc83096114daee1d26e004e7
Matt Dean <mdean400@gmail.com>
2021-03-19 23:06:02 -0600

    Expose redis primary endpoint as an output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -21,6 +21,15 @@ output "cache_configuration_endpoint_address" {
   value = aws_elasticache_replication_group.default.configuration_endpoint_address
 }
 
+output "cache_primary_endpoint_address" {
+  value = aws_elasticache_replication_group.default.primary_endpoint_address
+}
+
+# should exist but doesn't
+# output "cache_reader_endpoint_address" {
+#   value = aws_elasticache_replication_group.default.reader_endpoint_address
+# }
+
 output "mongo_shell_instance_ip" {
   value = aws_instance.service.public_ip
 }
