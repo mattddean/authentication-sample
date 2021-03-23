653a30307ab851177b9d349b5604038cdc9c024e
Matt Dean <mdean400@gmail.com>
2021-03-22 20:34:03 -0600

    Revert "Start reformatting Login such that it uses individual states for each form element"
    This reverts commit 15d19da774953ac8b3857a2951b58b0e3257c367.

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,66 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 import { Form, FormField, Button, Box, TextInput } from "grommet";
 import { NavLink as Link } from "react-router-dom";
-import { useUserContext } from "../hooks/useUserContext";
-import { UserContext } from "../contexts/UserContext";
-import { useUserContextValue } from "../hooks/useUserContextValue";
 
 export const Login = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const userContextValue = useUserContextValue(); // get user data
-  const { login } = useUserContext(); // update user data
+  const [value, setValue] = React.useState({});
   return (
     <Form
-      onReset={() => {
-        setFirstName("");
-      }}
+      value={value}
+      onChange={(nextValue: React.SetStateAction<any>) => setValue(nextValue)}
+      onReset={() => setValue({})}
       onSubmit={({ value }) => {
         console.log(value);
-        login(value.email, value.password);
       }}
     >
-      <FormField name="firstName" htmlFor="text-input-id" label="First Name">
-        <TextInput
-          id="text-input-id"
-          name="name"
-          value={firstName}
-          onChange={(newVal: React.ChangeEvent<HTMLInputElement>) => {
-            setFirstName(newVal.target.value);
-          }}
-        />
-      </FormField>
-      <FormField name="lastName" htmlFor="text-input-id" label="Last Name">
-        <TextInput
-          id="text-input-id"
-          name="name"
-          value={lastName}
-          onChange={(newVal: React.ChangeEvent<HTMLInputElement>) => {
-            setLastName(newVal.target.value);
-          }}
-        />
-      </FormField>
-      <FormField name="name" htmlFor="text-input-id" label="Email">
-        <TextInput
-          id="text-input-id"
-          name="username"
-          value={email}
-          onChange={(newVal: React.ChangeEvent<HTMLInputElement>) => {
-            setEmail(newVal.target.value);
-          }}
-        />
+      <FormField name="name" htmlFor="text-input-id" label="Username">
+        <TextInput id="text-input-id" name="name" />
       </FormField>
       <FormField name="name" htmlFor="text-input-id" label="Password">
-        <TextInput
-          id="text-input-id"
-          name="name"
-          value={password}
-          onChange={(newVal: React.ChangeEvent<HTMLInputElement>) => {
-            setPassword(newVal.target.value);
-          }}
-        />
+        <TextInput id="text-input-id" name="name" />
       </FormField>
       <Box direction="row" gap="medium">
         <Link to="/signup">
@@ -68,9 +25,7 @@ export const Login = () => {
         </Link>
       </Box>
       <Box direction="row" gap="medium">
-        <UserContext.Provider value={userContextValue}>
-          <Button type="submit" primary label="Submit" />
-        </UserContext.Provider>
+        <Button type="submit" primary label="Submit" />
       </Box>
     </Form>
   );
